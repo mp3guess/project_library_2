ef32721d4da125d223b25aac8538a2d68064cf29
mp3guess <tamrazov05@gmail.com>
2024-05-06 17:01:54 +0200

    change: config

--- src/main/java/config/SpringConfig.java
+++ src/main/java/config/SpringConfig.java
@@ -95,7 +95,7 @@ public class SpringConfig implements WebMvcConfigurer {
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
         em.setDataSource(dataSource());
-        em.setPackagesToScan("ru.alishev.springcourse.models");
+        em.setPackagesToScan("java.models");
 
         final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         em.setJpaVendorAdapter(vendorAdapter);
