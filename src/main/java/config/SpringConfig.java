39cfd86399ef06b26d2bdd70317fde82e30eb2e8
mp3guess <tamrazov05@gmail.com>
2024-05-06 17:00:30 +0200

    change: config

--- src/main/java/config/SpringConfig.java
+++ src/main/java/config/SpringConfig.java
@@ -7,11 +7,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -25,8 +27,11 @@ import java.util.Properties;
 
 @Configuration
 @ComponentScan("java")
-@EnableWebMvc
 @PropertySource("classpath:hibernate.properties")
+@EnableTransactionManagement
+@EnableJpaRepositories
+
+@EnableWebMvc
 public class SpringConfig implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
